eff52da987532a891ed022c60899f0e181b1a06d
Natalia <nataliko.ru@yandex.ru>
2024-09-13 22:28:11 +0300

    trying to deseryalize string to localdatetime3

--- stats-service/stats-service-dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ stats-service/stats-service-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -14,6 +14,6 @@ public class EndpointHitDto {
     private String uri;
     private String ip;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
