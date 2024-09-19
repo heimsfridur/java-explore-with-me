d369bb8725b0e023ea6be4957392d70e7b4839e8
Natalia <nataliko.ru@yandex.ru>
2024-09-19 22:49:20 +0300

    add public api for categories

--- main-service/src/main/java/ru/practicum/category/controller/PublicCategoryController.java
+++ main-service/src/main/java/ru/practicum/category/controller/PublicCategoryController.java
@@ -1,11 +1,30 @@
 package ru.practicum.category.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.category.dto.CategoryDto;
+import ru.practicum.category.service.PublicCategoryService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/categories")
 public class PublicCategoryController {
+
+    public final PublicCategoryService publicCategoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CategoryDto> getAll(@RequestParam(defaultValue = "0") int from,
+                                    @RequestParam(defaultValue = "10") int size) {
+        return publicCategoryService.getAll(from, size);
+    }
+
+    @GetMapping("/{catId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDto getAll(@PathVariable int catId) {
+        return publicCategoryService.getById(catId);
+    }
 }
--- main-service/src/main/java/ru/practicum/category/service/PublicCategoryService.java
+++ main-service/src/main/java/ru/practicum/category/service/PublicCategoryService.java
@@ -1,4 +1,11 @@
 package ru.practicum.category.service;
 
+import ru.practicum.category.dto.CategoryDto;
+
+import java.util.List;
+
 public interface PublicCategoryService {
+    List<CategoryDto> getAll(int from, int size);
+
+    CategoryDto getById(int catId);
 }
